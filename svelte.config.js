f0ab1b879d77d64de1d3ce657a155ebbe89b9aa6
IslamZaoui <zaoui.islam2021@gmail.com>
2023-09-08 05:02:07 +0100

    test runtime

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,21 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 import remarkUnwrapImages from 'remark-unwrap-images'
 import remarkToc from 'remark-toc'
 import rehypeSlug from 'rehype-slug'
-import {mdsvex} from 'mdsvex'
+import { mdsvex, escapeSvelte } from 'mdsvex'
+import shiki from 'shiki'
 
 /**@type {import(mdsvex).mdsvexOptions} */
 const mdsvexOptions = {
 	extensions:['.md'],
 	remarkPlugins: [remarkUnwrapImages, [remarkToc, { tight: true }]],
-	rehypePlugins: [rehypeSlug]
+	rehypePlugins: [rehypeSlug],
+	highlight: {
+		highlighter: async (code, lang = 'text') => {
+			const highlighter = await shiki.getHighlighter({ theme: 'poimandres' })
+			const html = escapeSvelte(highlighter.codeToHtml(code, { lang }))
+			return `{@html \`${html}\` }`
+		}
+	},
 }
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -22,7 +30,7 @@ const config = {
 	},
 	kit: {
 		adapter: adapter({
-			runtime:'edge'
+			runtime:'nodejs18.x'
 		}),
 		alias: {
 			'posts':'/src/posts'
