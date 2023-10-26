fc7ebc0468665aafc09955fe81f94a1beb68c386
IslamZaoui <zaoui.islam2021@gmail.com>
2023-10-26 18:45:30 +0100

    add robots, manifest and sitemap.xml

---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,45 @@
+import type { RequestHandler } from './$types';
+import * as config from '$lib/config'
+
+export const prerender = true
+
+export const GET: RequestHandler = async ({ fetch }) => {
+    const posts = (await (await fetch(`/en/API/getPosts?page=${1}&perPage=${100}`)).json()).posts as Post[];
+
+
+    const headers = { 'Content-Type': 'application/xml' }
+
+    const pages = [
+        '',
+        'posts',
+        ...posts.map((post) => "posts/"+post.slug),
+    ]
+
+    const sitemap = `
+	  <?xml version="1.0" encoding="UTF-8" ?>
+	  <urlset
+	    xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+	    xmlns:xhtml="https://www.w3.org/1999/xhtml"
+	    xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
+	    xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
+	    xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
+	    xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
+	  >
+	    ${pages
+            .map((page) => {
+                return `
+	          <url>
+	            <loc>${config.url}en/${page}</loc>
+	            <lastmod>${new Date().toISOString()}</lastmod>
+	          </url>
+              <url>
+	            <loc>${config.url}ar/${page}</loc>
+	            <lastmod>${new Date().toISOString()}</lastmod>
+	          </url>
+	        `
+            })
+            .join('')}
+	  </urlset>
+	`.trim()
+    return new Response(sitemap, { headers })
+}
