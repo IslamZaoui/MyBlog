c58da1b6ceddc0d15c81dbd77479c6a60919fc94
IslamZaoui <zaoui.islam2021@gmail.com>
2023-10-29 00:03:40 +0100

    change domain/url

--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -2,4 +2,4 @@ import { dev } from '$app/environment';
 
 export const title = 'Islam Zaoui';
 export const description = "Islam Zaoui's Personal blog";
-export const url = dev ? 'http://localhost:5173/' : 'https://islamzaoui.vercel.app/';
+export const url = dev ? 'http://localhost:5173/' : 'https://islamzaoui.top/';
