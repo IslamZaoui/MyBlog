854f38841b9763e68bb5c5bea405d8c6fd99b80b
IslamZaoui <zaoui.islam2021@gmail.com>
2023-11-02 00:12:47 +0100

    undo PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import { defineConfig } from 'vite';
 import { imagetools } from 'vite-imagetools';
 
@@ -12,27 +11,5 @@ export default defineConfig({
 				greedy: [/^hljs-/],
 			},
 		}),
-		imagetools(),
-		SvelteKitPWA({
-			srcDir: './src',
-			mode: 'production',
-			strategies: 'injectManifest',
-			filename: 'sw.ts',
-			scope: '/',
-			base: '/',
-			injectManifest: {
-				globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}']
-			},
-			devOptions: {
-				enabled: true,
-				type: 'module',
-				navigateFallback: '/'
-			},
-			kit: {}
-		})],
-	server: {
-		fs: {
-			allow: ['./static']
-		}
-	}
+		imagetools()]
 });
