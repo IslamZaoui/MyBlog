8ea3154901419b368014c228639802357139b240
IslamZaoui <zaoui.islam2021@gmail.com>
2023-11-23 22:07:42 +0100

    remove page config

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,10 +2,6 @@ import { loadLocaleAsync } from '$i18n/i18n-util.async';
 import { i18nObject } from 'typesafe-i18n';
 import type { LayoutLoad } from './$types';
 
-export const ssr = true;
-export const csr = true;
-export const prerender = true;
-
 export const load = (async ({ data }) => {
 	await loadLocaleAsync(data.Lang);
 	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
