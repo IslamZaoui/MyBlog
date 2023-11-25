d6c339b7b6d087d8111743d4ff592a759207e880
IslamZaoui <zaoui.islam2021@gmail.com>
2023-11-25 22:53:14 +0100

    try fix views fetch

--- src/routes/[lang]/posts/[post]/+page.server.ts
+++ src/routes/[lang]/posts/[post]/+page.server.ts
@@ -4,17 +4,23 @@ import type { PageServerLoad } from './$types';
 
 export const load = (async ({ fetch, url }) => {
     let views = "0"
-    const response = await fetch(`${StatsUrl}api/v0/stats/hits`, {
-        headers: {
-            'content-type': "application/json",
-            "Authorization": `Bearer ${env.GAOTCOUNTER_API_TOKEN}`
+    try {
+        const response = await fetch(`${StatsUrl}api/v0/stats/hits`, {
+            headers: {
+                'content-type': "application/jsoFFn",
+                "Authorization": `Bearer ${env.GAOTCOUNTER_API_TOKEN}`
+            }
+        })
+        if (response.ok) {
+            const hit = (await response.json()).hits.filter((hit: any) => hit.path === url.pathname)
+            if (hit.length === 1) {
+                views = hit[0].count
+                console.log("views: " + views)
+            }
         }
-    })
-    if (response.ok) {
-        const hit = (await response.json()).hits.filter((hit: any) => hit.path === url.pathname)
-        if (hit.length === 1) {
-            views = hit[0].count
-        }
+    }
+    catch (err) {
+        console.log('views fetch error:', err)
     }
     return {
         views
