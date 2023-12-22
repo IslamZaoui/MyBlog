ca5671a330647c7c6483bbc07f388a96f90ddce0
IslamZaoui <zaoui.islam2021@gmail.com>
2023-12-22 20:27:04 +0100

    add vercel Speed Insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,8 +14,10 @@
 
 	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
 	inject({ mode: dev ? 'development' : 'production' });
+	injectSpeedInsights();
 
 	// Highlight JS
 	import hljs from 'highlight.js';
